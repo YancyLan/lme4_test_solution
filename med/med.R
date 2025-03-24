3c0564df6d61cb41794472ee3d0f0c993d9a87ef
Wenjie Lan <wenjielan2022@outlook.com>
2025-03-23 20:19:09 -0400

    write up for task 2

--- med/med.R
+++ med/med.R
@@ -8,12 +8,14 @@ lf <- lFormula(Reaction ~ Days + (Days | Subject), data = sleepstudy,
 low <- lf$reTrms$lower
 devfun <- mkLmerDevfun(lf$fr, lf$X, lf$reTrms)
 
+# 1: Wrapper (use only the diagonal elements)
 diag_wrapper <- function(sd) {
   theta <- numeric(length(low))
   theta[which(low == 0)] <- sd
   devfun(theta)
 }
 
+# 2: Optimize the diagonal parameters with nloptwrap
 n_diag <- sum(low == 0)
 opt_res <- nloptwrap(
   par = rep(0, n_diag),
@@ -23,8 +25,10 @@ opt_res <- nloptwrap(
   control = list(algorithm = "NLOPT_LN_BOBYQA", maxeval = 1000)
 )
 
+# 3: Construct the final lme4 model with optimized parameters
 fit <- mkMerMod(rho = environment(devfun), opt = opt_res, reTrms = lf$reTrms, fr = lf$fr, mc = match.call())
 print(fit)
 
+# 4: Compare with glmmTMB fit
 tmb_fit <- glmmTMB(Reaction ~ Days + diag(Days | Subject), data = sleepstudy, REML = TRUE)
 print(summary(tmb_fit))
